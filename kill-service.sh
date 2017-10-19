7997ca0aca98359f039ad48012d0eff94d74c9ac
cobainc0 <yancoba.thompson.1@city.ac.uk>
2017-10-19 21:00:44 +0100

    Clean up kill and run scripts after merge

--- kill-service.sh
+++ kill-service.sh
@@ -5,16 +5,9 @@ GID=`ps -ef | grep $JAVA_HOME/bin/java | grep "GradleWrapperMain"  | grep -v gre
 
 if [[ ! -z "$BID" ]] && [[ ! -z "$GID" ]]
 then 
-<<<<<<< HEAD
 	kill -9 $GID &&
 	echo "Killing Gradle Application" &&
 	kill -9 $BID &&
 	echo "Killing Beam Application" &&
-	kill -9 $BID
-=======
-	kill -9 $BID &&
-	kill -9 $GID &&
-	echo "Killing Beam & Gradle Application"
->>>>>>> 82d44f46f0f2233ea8cd30badb0e039a5d0cab35
 fi
 
--- startup.sh
+++ startup.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
+
 ./kill-service.sh &&
 echo "...Killing any running service and launching Beam on port http://localhost:53000/api :)" &&
 echo "try ..." &&
 echo "http://localhost:53000/api/jsonResponse" &&
 echo "http://localhost:53000/api/stringResponse" &&
+echo 'Beam starting on port 53000 :)' &&
 ./gradlew run
-
-=======
-
-./gradlew run && echo 'Beam starting on port 53000 :)'
->>>>>>> 82d44f46f0f2233ea8cd30badb0e039a5d0cab35
